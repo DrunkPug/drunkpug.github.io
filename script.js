5a4deca76fcf9b2eb5ac5ecbf0be2dfc3a2dcd13
DrunkPug <91783509+DrunkPug@users.noreply.github.com>
2023-09-29 09:08:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function writePoem () {
+    let name = document.getElementById("input-name").value;
+    if ( name == "Геральт") { 
+        document.querySelector(".poem-block").style.backgroundImage = "url('geralt4.png')";
+        document.getElementById("poem-text2").textContent = `Oh KURWA!`;
+    } else if ( name != "") {
+        document.querySelector(".poem-block").style.backgroundImage = "url('geralt3.png')";
+        document.getElementById("poem-text1").textContent = `${name}, ${name} хер моржовый,`;
+        document.getElementById("poem-text2").textContent = `${name}, ${name} вредный хуй!`;
+         };
+
+    document.querySelector(".main-block").style.display = "none";
+    document.querySelector(".poem-block").style.display = "block";
+}
+
+function newPoem() {
+    document.querySelector(".main-block").style.display = "block";
+    document.querySelector(".poem-block").style.display = "none";
+    document.getElementById("input-name").value = "Введите имя";
+    document.getElementById("poem-text").textContent = "";
+}
+
+function clearInput() {
+    document.getElementById("input-name").value = "";
+}
+
+document.getElementById("btn-write").addEventListener('click', writePoem);
+document.getElementById("btn-new").addEventListener('click', newPoem);
+document.getElementById("input-name").addEventListener('click', clearInput);
